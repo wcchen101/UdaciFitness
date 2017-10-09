9c94de34fc5ea69e77273204f47d31741aa764c8
Wei-Chung Chen <wcchen101@gmail.com>
2017-10-08 23:42:58 -0700

    feat: add text button component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
 
 function SubmitBtn({ onPress }) {
   return (
@@ -66,13 +68,35 @@ export default class AddEntry extends Component {
       [metric]: value
     }))
   }
+  reset = () => {
+    const key = timeToString()
+    // Update Redux
+
+    // Route to Home
+
+    // Update "DB"
+  }
   render() {
     const metaInfo = getMetricMetaInfo()
     const state = this.state.bike
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name={'ios-happy-outline'}
+            size={100}
+          />
+          <Text>You already logged your information for today.</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
+
     return (
       <View>
         <Text>{JSON.stringify(this.state)}</Text>
-
         <DateHeader date={(new Date()).toLocaleDateString()}/>
 
         {Object.keys(metaInfo).map((key) => {
