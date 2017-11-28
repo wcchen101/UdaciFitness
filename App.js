c04f23678dc7f31805686a0e066377d752c2e11a
Wei-Chung Chen <wcchen101@gmail.com>
2017-11-27 22:09:12 -0800

    feat: stack navigation, entry details, live components

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { View, Platform } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-import { TabNavigator } from 'react-navigation'
-import { purple, white } form './utils/colors'
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
+import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
 
+function UdaciStatusBar ({backgroundColor, ... props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
 const Tabs = TabNavigator({
   History: {
     screen: History,
@@ -25,6 +35,13 @@ const Tabs = TabNavigator({
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30}
       color={tintColor}/>
     },
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+    tabBarLabel: 'Live',
+    tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
+    }
   },
 },
 {
@@ -47,15 +64,30 @@ const Tabs = TabNavigator({
  }
 })
 
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    }
+  }
+})
 
 export default class App extends React.Component {
 
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={{flex: 1}}>
-          <Tab />
-        </View>
+         <View style={{flex: 1}}>
+           <UdaciStatusBar backgroundColor={purple} barStyle='light-content'/>
+           <MainNavigator />
+         </View>
       </Provider>
     );
   }
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -10,6 +10,7 @@ import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { purple, white } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn({ onPress }) {
   return (
@@ -46,6 +47,7 @@ class AddEntry extends Component {
       eat: 0,
     }))
     //Navigate to home
+    this.toHome()
 
     submitEntry({key, entry})
 
@@ -85,8 +87,12 @@ class AddEntry extends Component {
       [key]: getDailyReminderValue()
     }))
     // Route to Home
+    this.toHome()
 
     removeEntry(key)
+  }
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({key: 'AddEntry'}))
   }
   render() {
     const metaInfo = getMetricMetaInfo()
